763893cf3366ae9c1585c059fd4aee5e6b16d2aa
SandeepChowdary2305 <154881089+SandeepChowdary2305@users.noreply.github.com>
2024-11-01 14:03:12 +0530

    Step 1

--- about.php
+++
@@ -1,4 +0,0 @@
-<div class="content-section">
-    <h1>About Us</h1>
-    <p>We are a team of dedicated professionals providing expert IT consulting services. With years of experience in the field, we help businesses achieve their technology and growth goals.</p>
-</div>
